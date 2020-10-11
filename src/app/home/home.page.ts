546bbcd90c8721054deaae2796eb82475df1a939
hanscarreon02 <hansmobiledev@gmail.com>
2020-10-11 17:29:55 +0800

    add score board and sync data

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,7 +1,8 @@
 import { Router } from '@angular/router';
 import { Component } from '@angular/core';
 import { DataApiService } from '../data-api.service';
-import { NavController } from '@ionic/angular';
+import { NavController, LoadingController } from '@ionic/angular';
+import { Location } from '@angular/common';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -12,7 +13,10 @@ isRoot = true;
   constructor(
     private datas: DataApiService,
     private router: Router,
-    private  nav : NavController
+    private  nav : NavController,
+    private location: Location,
+    private loadingController: LoadingController
+
   ) {
     console.log( this.router.url);
     if( this.router.url == "home/teams" || this.router.url == "home/news" )
@@ -27,6 +31,32 @@ isRoot = true;
 
   naviPage(destination){
     this.nav.navigateForward([destination]);
+  }
+  updateData(){
+    this.presentLoading()
+  }
+  async presentLoading() {
+    const loading = await this.loadingController.create({
+      cssClass: 'my-custom-class',
+      message: 'Fetching Data',
+      duration: 2000
+    });
+    await loading.present();
+
+    const { role, data } = await loading.onDidDismiss();
+    console.log('Loading dismissed!');
+    window.location.reload();
+
+  }
+
+  showLoader() {
+
+    this.loadingController.create({
+      message: 'Please wait...'
+    }).then((res) => {
+      res.present();
+    });
+
   }
 
   
--- src/app/home/scores/scores.component.ts
+++ src/app/home/scores/scores.component.ts
@@ -1,6 +1,7 @@
+import { LoadingController } from '@ionic/angular';
 import { DataApiService } from './../../data-api.service';
 import { Component, OnInit } from '@angular/core';
-
+import { Location } from '@angular/common';
 @Component({
   selector: 'app-scores',
   templateUrl: './scores.component.html',
@@ -9,22 +10,60 @@ import { Component, OnInit } from '@angular/core';
 export class ScoresComponent implements OnInit {
 matchResult = [];
 headLine;
+isLoading = false;
   constructor(
-    private apiData: DataApiService
+    private apiData: DataApiService,
+    private location: Location,
+    public loadingController: LoadingController
+
+
   ) { }
 
   ngOnInit() {
+    
+    this.showLoader()
     this.readScore();
   }
 
   readScore(){
     this.apiData.getScores().subscribe((data)=> {
-      console.log(data.events[0].name);
+      console.log(data.events);
       // console.log(data.events[0].competitions[0].notes[0].headline);
       this.headLine = data.events[0].competitions[0].notes[0].headline
-      this.matchResult = data.events[0];
+      this.matchResult = data.events;
+      // this.io
+      if(data){
+        setTimeout(() => {
+            this.hideLoader();
+            this.isLoading = true
+
+        }, 1000);
+      }
       console.log("score");
     })
   }
+  backClicked() {
+    this.location.back()
+  }// history back 
+  showLoader() {
+
+    this.loadingController.create({
+      message: 'Please wait...'
+    }).then((res) => {
+      res.present();
+    });
+
+  }
+  // Show the loader for infinite time
+
+  hideLoader() {
+
+    this.loadingController.dismiss().then((res) => {
+      console.log('Loading dismissed!', res);
+    }).catch((error) => {
+      console.log('error', error);
+    });
+  }
+  // Hide the loader if already created otherwise return error
 
 }
